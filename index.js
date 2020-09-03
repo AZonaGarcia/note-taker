324bc48008da74c9d1cd81dfa0cc26de0871b4bb
Bonus Mom <AZonaGarcia@aol.com>
2020-09-03 01:12:21 -0400

    fixing route

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ var PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/ *", function(req, res) {
+app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
-  });
-  
-  app.get("/notes", function(req, res) {
+});
+
+app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "notes.html"));
-  });
+});
