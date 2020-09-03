e04598f6573c5fb2f9bb3b1d60106e6c4cc0b97b
Bonus Mom <AZonaGarcia@aol.com>
2020-09-03 01:24:40 -0400

    attempting to fix js file

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "public/index.html"));
+    res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "public/notes.html"));
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
