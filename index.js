d2529ef500e737da5113f81efc189639d0c45f80
Bonus Mom <AZonaGarcia@aol.com>
2020-09-03 01:20:05 -0400

    removed rogue s

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var path = require("path");
 // =============================================================
 var app = express();
 var PORT = process.env.PORT || 3001;
-s
+
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
